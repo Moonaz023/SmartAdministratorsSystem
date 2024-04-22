a34d6433e8dc92f4d61d1a9c08053d5f904aa97e
Moonaz023 <moonaz.cse6.bu@gmail.com>
2024-04-22 12:15:10 +0600

    correct upload path

--- src/main/java/com/sas/service/NoticeServiceImp.java
+++ src/main/java/com/sas/service/NoticeServiceImp.java
@@ -1,15 +1,6 @@
 package com.sas.service;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
+//	private static final String UPLOAD_DIR = "src\\main\\resources\\static\\notics";
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
@@ -24,130 +15,133 @@ import org.springframework.web.multipart.MultipartFile;
 import com.sas.entity.NoticeEntity;
 import com.sas.repository.NoticeRepository;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @Service
 public class NoticeServiceImp implements NoticeService {
 
-	@Autowired
-	private NoticeRepository noticeRepository;
-	private static final String UPLOAD_DIR = "src\\main\\resources\\static\\notics";
-	public final String UPLOAD_DIR2 = new ClassPathResource("static/notics").getFile().getAbsolutePath();
-	public NoticeServiceImp()throws IOException {
-		
-	}
-//	private static final String UPLOAD_DIR = "notics";
+    @Autowired
+    private NoticeRepository noticeRepository;
 
-	@Override
-	public void insertNotice(NoticeEntity notice, MultipartFile file) {
-		// TODO Auto-generated method stub
-		try {
-			String originalFileName = StringUtils.cleanPath(file.getOriginalFilename());
-			notice.setFileName(originalFileName);
-			Path uploadPath = Paths.get(UPLOAD_DIR2);
+    public final String UPLOAD_DIR2 = new ClassPathResource("static/notics").getPath();
 
-			// If the directory doesn't exist, create it
-			if (!Files.exists(uploadPath)) {
-				Files.createDirectories(uploadPath);
-			}
+    public NoticeServiceImp() throws IOException {
+    }
 
-			// Save the file to the server
-			try (InputStream inputStream = file.getInputStream()) {
-				Files.copy(inputStream, uploadPath.resolve(originalFileName), StandardCopyOption.REPLACE_EXISTING);
-			}
+    @Override
+    public void insertNotice(NoticeEntity notice, MultipartFile file) {
+        try {
+            String originalFileName = StringUtils.cleanPath(file.getOriginalFilename());
+            notice.setFileName(originalFileName);
+            Path uploadPath = Paths.get(UPLOAD_DIR2);
 
-			System.out.println("Notice file uploaded successfully: " + originalFileName);
+            // If directory doesn't exist, create it
+            if (!Files.exists(uploadPath)) {
+                Files.createDirectories(uploadPath);
+            }
 
-		} catch (IOException e) {
-			e.printStackTrace();
+            // Save file to server
+            try (InputStream inputStream = file.getInputStream()) {
+                Files.copy(inputStream, uploadPath.resolve(originalFileName), StandardCopyOption.REPLACE_EXISTING);
+            }
 
-		}
-		noticeRepository.save(notice);
-	}
+            System.out.println("Notice file uploaded successfully: " + originalFileName);
 
-	@Override
-	public List<NoticeEntity> fetchNotice() {
-		Sort sort = Sort.by(Sort.Direction.DESC, "id");
-		List<NoticeEntity> listOfNotice = noticeRepository.findAll(sort);
-		return listOfNotice;
-	}
+        } catch (IOException e) {
+            e.printStackTrace();
 
-	private static final Map<String, MediaType> CONTENT_TYPE_MAP = new HashMap<>();
+        }
+        noticeRepository.save(notice);
+    }
 
-	static {
-		CONTENT_TYPE_MAP.put("pdf", MediaType.APPLICATION_PDF);
-		CONTENT_TYPE_MAP.put("jpg", MediaType.IMAGE_JPEG);
-		CONTENT_TYPE_MAP.put("jpeg", MediaType.IMAGE_JPEG);
-		CONTENT_TYPE_MAP.put("png", MediaType.IMAGE_PNG);
-		CONTENT_TYPE_MAP.put("csv", MediaType.TEXT_PLAIN);
-		CONTENT_TYPE_MAP.put("xls", MediaType.valueOf("application/vnd.ms-excel"));
-		// Add more content types as needed
-	}
+    @Override
+    public List<NoticeEntity> fetchNotice() {
+        Sort sort = Sort.by(Sort.Direction.DESC, "id");
+        List<NoticeEntity> listOfNotice = noticeRepository.findAll(sort);
+        return listOfNotice;
+    }
 
-	@Override
-	public ResponseEntity<Resource> openFileInNewTab(String fileName) throws IOException {
-		Resource resource = new ClassPathResource("static/notics/" + fileName);
-		//Resource resource = new ClassPathResource("notics/" + fileName);
+    private static final Map<String, MediaType> CONTENT_TYPE_MAP = new HashMap<>();
 
-		String fileExtension = getFileExtension(fileName);
-		MediaType contentType = CONTENT_TYPE_MAP.getOrDefault(fileExtension.toLowerCase(),
-				MediaType.APPLICATION_OCTET_STREAM);
+    static {
+        CONTENT_TYPE_MAP.put("pdf", MediaType.APPLICATION_PDF);
+        CONTENT_TYPE_MAP.put("jpg", MediaType.IMAGE_JPEG);
+        CONTENT_TYPE_MAP.put("jpeg", MediaType.IMAGE_JPEG);
+        CONTENT_TYPE_MAP.put("png", MediaType.IMAGE_PNG);
+        CONTENT_TYPE_MAP.put("csv", MediaType.TEXT_PLAIN);
+        CONTENT_TYPE_MAP.put("xls", MediaType.valueOf("application/vnd.ms-excel"));
+        // Add more -----
+    }
 
-		HttpHeaders headers = new HttpHeaders();
-		headers.add(HttpHeaders.CONTENT_TYPE, contentType.toString());
+    @Override
+    public ResponseEntity<Resource> openFileInNewTab(String fileName) throws IOException {
+        Resource resource = new ClassPathResource("static/notics/" + fileName);
 
-		return ResponseEntity.ok().headers(headers).body(resource);
-	}
+        String fileExtension = getFileExtension(fileName);
+        MediaType contentType = CONTENT_TYPE_MAP.getOrDefault(fileExtension.toLowerCase(),
+                MediaType.APPLICATION_OCTET_STREAM);
 
-	private String getFileExtension(String fileName) {
-		int lastDotIndex = fileName.lastIndexOf('.');
-		if (lastDotIndex > 0 && lastDotIndex < fileName.length() - 1) {
-			return fileName.substring(lastDotIndex + 1);
-		}
-		return "";
-	}
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_TYPE, contentType.toString());
 
-	@Override
-	public void deleteNotice(long id) {
-		noticeRepository.deleteById(id);
-		
-	}
+        return ResponseEntity.ok().headers(headers).body(resource);
+    }
 
-	@Override
-	public void updateNotice(NoticeEntity notice, MultipartFile file) {
-	    try {
-	        if (file != null && !file.isEmpty()) {
-	            String originalFileName = StringUtils.cleanPath(file.getOriginalFilename());
-	            Path uploadPath = Paths.get(UPLOAD_DIR2);
+    private String getFileExtension(String fileName) {
+        int lastDotIndex = fileName.lastIndexOf('.');
+        if (lastDotIndex > 0 && lastDotIndex < fileName.length() - 1) {
+            return fileName.substring(lastDotIndex + 1);
+        }
+        return "";
+    }
 
-	            // If the directory doesn't exist, create it
-	            if (!Files.exists(uploadPath)) {
-	                Files.createDirectories(uploadPath);
-	            }
+    @Override
+    public void deleteNotice(long id) {
+        noticeRepository.deleteById(id);
+    }
 
-	            // Delete the old file if it exists
-	            String oldFileName = notice.getFileName();
-	            if (oldFileName != null && !oldFileName.isEmpty()) {
-	                Path oldFilePath = uploadPath.resolve(oldFileName);
-	                if (Files.exists(oldFilePath)) {
-	                    Files.delete(oldFilePath);
-	                    System.out.println("Old notice file deleted successfully: " + oldFileName);
-	                }
-	            }
+    @Override
+    public void updateNotice(NoticeEntity notice, MultipartFile file) {
+        try {
+            if (file != null && !file.isEmpty()) {
+                String originalFileName = StringUtils.cleanPath(file.getOriginalFilename());
+                Path uploadPath = Paths.get(UPLOAD_DIR2);
 
-	            // Save the new file to the server
-	            try (InputStream inputStream = file.getInputStream()) {
-	                Files.copy(inputStream, uploadPath.resolve(originalFileName), StandardCopyOption.REPLACE_EXISTING);
-	            }
+                // If directory doesn't exist, create it
+                if (!Files.exists(uploadPath)) {
+                    Files.createDirectories(uploadPath);
+                }
 
-	            System.out.println("Notice file updated successfully: " + originalFileName);
-	            notice.setFileName(originalFileName);
-	        }
-	    } catch (IOException e) {
-	        e.printStackTrace();
-	    }
-	    
-	    noticeRepository.save(notice);
-	}
+                // Delete old file if exists
+                String oldFileName = notice.getFileName();
+                if (oldFileName != null && !oldFileName.isEmpty()) {
+                    Path oldFilePath = uploadPath.resolve(oldFileName);
+                    if (Files.exists(oldFilePath)) {
+                        Files.delete(oldFilePath);
+                        System.out.println("Old notice file deleted successfully: " + oldFileName);
+                    }
+                }
 
+                // Save new file to server
+                try (InputStream inputStream = file.getInputStream()) {
+                    Files.copy(inputStream, uploadPath.resolve(originalFileName), StandardCopyOption.REPLACE_EXISTING);
+                }
 
+                System.out.println("Notice file updated successfully: " + originalFileName);
+                notice.setFileName(originalFileName);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
+        noticeRepository.save(notice);
+    }
 }
